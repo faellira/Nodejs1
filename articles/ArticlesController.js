19cce942f14073efa8adb093ce982a0e6a02806a
Rafael lira <rafaelirasouza@gmail.com>
2023-10-29 22:29:14 -0300

    first commit

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('../articles/Article');
+const slugify = require('slugify');
 
-router.get('/articles', (req, res)=>{
-    res.send("Página de Artigos");
-});
+router.get('/admin/articles', (req, res) =>{
+    Article.findAll({include: [{model: Category}]}).then(articles => {
+        res.render('admin/articles/index', {articles : articles})
+    }); 
+})
+
+router.get('/admin/articles/new', (req, res) => {
+    Category.findAll().then(categories =>{
+        res.render('admin/articles/new', {categories : categories})
+    })
+})
+
+router.post('/articles/save', (req, res) =>{
+    let title = req.body.title;
+    let category = req.body.selectCategories;
+    let bodyArticle = req.body.articleBody;
+
+        Article.create({
+            title: title,
+            slug: slugify(title),
+            body: bodyArticle,
+            categoryId: category  
+        }).then(() =>{
+            res.redirect('/admin/articles')
+        });
+})
+
 
 module.exports = router;
